fe9037b3231556fad65c54cdb815f91a20fd9e17
oliveira7250 <99373026+oliveira7250@users.noreply.github.com>
2022-05-14 18:58:38 -0300

    .

--- lab05/src/pt/c40task/l05wumpus/Componente.java
+++ lab05/src/pt/c40task/l05wumpus/Componente.java
@@ -1,3 +1,58 @@
+package pt.c40task.l05wumpus;
+import java.util.Random;
+
 public class Componente {
-    ;
+    private Caverna caverna;
+    protected int coordLinhaSala;
+    protected int coordColunaSala;
+
+    public Caverna getCaverna() {
+        return caverna;
+    }
+
+    public int getCoordLinha() {
+        return coordLinhaSala;
+    } 
+    public int getCoordColuna() {
+        return coordColunaSala;
+    }
+
+}
+
+public class Heroi extends Componente {
+    private int qntFlecha;
+    private boolean flechaPronta;
+
+    public void equiparFlecha() {
+        qntFlecha -= 1;
+        flechapronta = true;
+    }
+
+    public int flechasDisp() {
+        return qntFlecha;
+    }
+
+    public boolean podeAtirar() {
+        if (flechasDisp() == 0)
+            flechaPronta = false;
+        return flechaPronta;
+    }
+
+
+    public void atirarFlecha() {
+        flechaPronta = false;
+    }
+
+    public boolean matarWumpus() {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+
+    public void capturarOuro() {
+
+    }
+
+    public boolean capturarOuro() {
+        return hero.caverna.getSala(i, j).getComp() == 'o';
+    }
 }
