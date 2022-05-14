924cdc375ec8a4ff00e3e8cbdcc8d957fb655fa5
oliveira7250 <99373026+oliveira7250@users.noreply.github.com>
2022-05-14 18:59:03 -0300

    .

--- lab05/src/pt/c40task/l05wumpus/Sala.java
+++ lab05/src/pt/c40task/l05wumpus/Sala.java
@@ -1,3 +1,98 @@
-public class Sala {
-    ;
-}
+# Classes implementadas por nós em UML
+ 
+<i>@ significa: a ser decidido</i>
+ 
+## Caverna
+ 
+Caverna   |
+----------|
+Atributos:|
+\- salas: Sala[][]|
+Metodos:|
+\@ criarSalas(): void|
+\@ conectarComponente(): void|
+\@ getSala(): Sala|
+
+## Sala
+Sala|
+----------|
+Atributos:|
+\- comp: Componente[]|
+Metodos:|
+\@ Sala()|
+\@ verificarComp(): boolean|
+\@ determinarPrior(); int|
+\@ setComp(): void|
+\@ getComp(): Componente|
+ 
+## Componente
+Componente|
+--------|
+Atributos:
+\@ caverna: Caverna|
+\@ coordLinhaSala: int|
+\@ coordColunaSala: int|
+Metodos:|
+\+ getcaverna();
+\+ getCoordLinha: int|
+\+ getCoordColuna: int|
+ 
+HERDEIROS:
+ 
+Heroi|
+-----|
+Atributos:|
+\- qntFlecha: int|
+\- flechaDisp: boolean|
+Metodos:|
+\- podeFlecha(): boolean|
+\- equiparFlecha(): void|
+\- atirarFlecha(): void|
+\- capturarOuro(): void|
+\- capturarOuro(): boolean|
+\- cairBuraco(): boolean|
+\- matarWumpus(): boolean|
+\- mover(i: int, j: int): void|
+\@ getPontuacao(): int|
+\@ setPontuacao(): void|
+ 
+Wumpus|
+------|
+Atributos:|
+\ @criarFedores(): void
+
+Fedor|
+-----|
+Atributos:|
+Metodos:|
+
+Buraco|
+------|
+Atributos:|
+Metodos:|
+\ @criarBrisas(): void|
+
+Brisa|
+Atributos:|
+Metodos:|
+
+Ouro|
+----|
+Atributos:|
+Metodos:|
+
+
+
+## Controle
+|Controle|
+---------|
+Atributos:
+\- command: String|
+\- hero: Heroi|
+Metodos:
+\- Controle(hero: Heroi)|
+\- receberMovimento(command: String): void|
+\- executarMovimento(): void|
+\- pontuar(): void|
+
+## Montador
