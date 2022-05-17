d2616ea727eb8098f27fd93b7616d6d14df50c1e
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-05-17 14:14:56 -0300

    Update Controle.java

--- lab05/src/pt/c40task/l05wumpus/Controle.java
+++ lab05/src/pt/c40task/l05wumpus/Controle.java
@@ -47,10 +47,7 @@ public class Controle {
 				else
 					pontuacao += 500;
 			}
-		} if (hero.capturarRecompensa() == true) { // eh que esse metodo precisa ser do componente, se n n consigo usar
-			// ele na Sala, e tipo eu acho q se o jogador n pegou o ouro paciencia, uma hora ele vai precisar pegar, a nao
-			// ser que ele saia do jogo.
-			// o que a gente tem que tratar na verdade eh se o jogador aperta algum botao que n seja do comando do jogo
+		} if (hero.capturarRecompensa() == true) {
 			pontuacao += 1000;
 		}
 	}
