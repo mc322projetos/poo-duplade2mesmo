c18314afadaa0b142e3427afd9971120d7c68367
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-05-17 19:41:15 -0300

    Update Componente.java

--- lab05/src/pt/c40task/l05wumpus/Componente.java
+++ lab05/src/pt/c40task/l05wumpus/Componente.java
@@ -6,6 +6,11 @@ public class Componente {
     protected int coordLinhaSala;
     protected int coordColunaSala;
 
+    Componente(Caverna caverna, int coordLinhaSala, int coordColunaSala) { // criei o construtor, pq se n n da pra usar os atributos da classe
+        this.caverna = caverna;
+        this.coordLinhaSala = coordLinhaSala;
+        this.coordColunaSala = coordColunaSala;
+    }
 
     public Caverna getCaverna() {
         return caverna;
@@ -18,6 +23,15 @@ public class Componente {
         return coordColunaSala;
     }
 
-    public boolean executarAcao() {}
+    public boolean lutarContraWumpus() {
+        int i = coordLinhaSala;
+        int j = coordColunaSala;
+        if (getCaverna().getSala(i, j).getMaiorPrior(1) == W) { // tem um problema aqui, a gente precisa dar um jeito de fazer essa verificacao
+            // sem usar o objeto W
+            Random random = new Random();
+            return random.nextBoolean();
+        }
+        return false;
+    }
 
 }
