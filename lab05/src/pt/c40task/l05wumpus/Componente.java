2cea34f23ee39d7ba7df535de2fff19e3ef3c5d6
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-05-17 19:46:31 -0300

    Update Componente.java

--- lab05/src/pt/c40task/l05wumpus/Componente.java
+++ lab05/src/pt/c40task/l05wumpus/Componente.java
@@ -26,7 +26,7 @@ public class Componente {
     public boolean lutarContraWumpus() {
         int i = coordLinhaSala;
         int j = coordColunaSala;
-        if (getCaverna().getSala(i, j).getMaiorPrior(1) == W) { // tem um problema aqui, a gente precisa dar um jeito de fazer essa verificacao
+        if (caverna.getSala(i, j).getMaiorPrior(1) == W) { // tem um problema aqui, a gente precisa dar um jeito de fazer essa verificacao
             // sem usar o objeto W
             Random random = new Random();
             return random.nextBoolean();
