881013b1f1d764f0bffaca2524181e1c8621067e
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-05-18 19:17:11 -0300

    Update Caverna.java

--- lab05/src/pt/c40task/l05wumpus/Caverna.java
+++ lab05/src/pt/c40task/l05wumpus/Caverna.java
@@ -1,3 +1,19 @@
+package pt.c40task.l05wumpus;
+
 public class Caverna {
-    ;
+	private Sala[][] caverna;
+
+	Caverna() {
+		this.caverna = new Salas[4][4];
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				caverna[i][j] = new Sala();
+			}
+		}
+	}
+
+	public Sala[][] getCaverna() {
+		return caverna;
+	}
 }
+
