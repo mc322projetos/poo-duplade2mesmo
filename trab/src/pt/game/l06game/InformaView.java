1cec07f592362d3f6ad2d40b33525604546e88aa
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-06-13 20:23:56 -0300

    Delete InformaView.java

--- trab/src/pt/game/l06game/InformaView.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public interface InformaView {
-	public boolean play(); // informa para o componente view se esta td pronto para iniciar o jogo
-	public boolean mudou();
-	public int[] oQueMudou();
-}
