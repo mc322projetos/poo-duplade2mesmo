7005aededb9fa989f98b7b739443717c2a8e3451
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-06-13 23:08:51 -0300

    Update Toca.java

--- trab/src/pt/game/l06game/model/Toca.java
+++ trab/src/pt/game/l06game/model/Toca.java
@@ -1,9 +1,14 @@
 package model;
 
 public class Toca extends Atores {
+	private char tipo;
 	
-	Toca(Labirinto maze) {
+	Toca(Labirinto maze, char tipo) {
 		super(maze);
+		this.tipo = tipo;
 	}
-
+	
+	public char getTipo() {
+		return tipo;
+	}
 }
