2f1b8e0ebb8d361d8d79a6a9aece561b16477210
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-06-14 12:34:04 -0300

    Update IModelView.java

--- trab/src/pt/game/l06game/model/IModelView.java
+++ trab/src/pt/game/l06game/model/IModelView.java
@@ -1,1 +1,7 @@
+package model;
 
+public interface IModelView {
+	public Celula[][] informaMatriz(); // informa para o componente view se esta td pronto para iniciar o jogo
+	public boolean mudou();
+	public int[] oQueMudou();
+}
