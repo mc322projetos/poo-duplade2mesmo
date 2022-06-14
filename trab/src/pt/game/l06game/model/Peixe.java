0b395f708080965fc6e10e48caeb197e48e79c6d
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-06-13 23:08:17 -0300

    Update Peixe.java

--- trab/src/pt/game/l06game/model/Peixe.java
+++ trab/src/pt/game/l06game/model/Peixe.java
@@ -1,8 +1,14 @@
 package model;
 
 public class Peixe extends Atores {
+	private char tipo;
 	
-	Peixe(Labirinto maze) {
+	Peixe(Labirinto maze, char tipo) {
 		super(maze);
+		this.tipo = tipo;
+	}
+	
+	public char getTipo() {
+		return tipo;
 	}
 }
