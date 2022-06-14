7e205a425685e1f66a1b1cc58bf4b564a3a7c7e7
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-06-13 23:05:25 -0300

    Update Cachorro.java

--- trab/src/pt/game/l06game/model/Cachorro.java
+++ trab/src/pt/game/l06game/model/Cachorro.java
@@ -1,9 +1,14 @@
 package model;
 
 public class Cachorro extends Atores {
+	private char tipo;
 	
-	Cachorro(Labirinto maze) {
+	Cachorro(Labirinto maze, char tipo) {
 		super(maze);
+		this.tipo = tipo;
 	}
-
+	
+	public char getTipo() {
+		return tipo;
+	}
 }
