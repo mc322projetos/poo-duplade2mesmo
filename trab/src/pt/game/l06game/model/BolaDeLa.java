55676710e7caf5f92f7a046b3f2167854bdc8d1d
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-06-13 23:04:50 -0300

    Update BolaDeLa.java

--- trab/src/pt/game/l06game/model/BolaDeLa.java
+++ trab/src/pt/game/l06game/model/BolaDeLa.java
@@ -1,9 +1,14 @@
 package model;
 
 public class BolaDeLa extends Atores {
+	private char tipo;
 	
-	BolaDeLa(Labirinto maze) {
+	BolaDeLa(Labirinto maze, char tipo) {
 		super(maze);
+		this.tipo = tipo;
 	}
-
+	
+	public char getTipo() {
+		return tipo;
+	}
 }
