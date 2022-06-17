52b7f555e45decfc31fe2db14f6a607e105fb5bf
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-06-17 13:09:35 -0300

    Update Gato.java

--- trab/src/pt/game/l06game/model/Gato.java
+++ trab/src/pt/game/l06game/model/Gato.java
@@ -1,11 +1,12 @@
 package model;
 
-public class Gato extends Atores {
+public class Gato implements Atores {
 	private int qtdPeixes;
 	private char tipo;
+	private Labirinto maze;
 	
 	Gato(Labirinto maze, int qtdPeixes, char tipo) {
-		super(maze);
+		this.maze = maze;
 		this.qtdPeixes = qtdPeixes;
 		this.tipo = tipo;
 	}
@@ -20,5 +21,17 @@ public class Gato extends Atores {
 	
 	public char getTipo() {
 		return tipo;
+	}
+	
+	public int[] mover() { // preciso das coordenadas de movimento dadas pelo control na interface requerida para passar
+		// para o labirinto e ele decidir se o gato pode se mover ou n, que no caso eh esse i,j
+		int[] movimento = new int[2];
+		movimento[0] = i;
+		movimento[1] = j;
+		return movimento;
+	}
+	
+	public void capturar() {
+		
 	}
 }
