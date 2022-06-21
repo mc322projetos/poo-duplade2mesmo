ffe7631f4d110fd978acd70eea39c7b6e33a513f
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-06-21 20:32:22 -0300

    Update JanelaJogo.java

--- trab/src/pt/game/l06game/view/JanelaJogo.java
+++ trab/src/pt/game/l06game/view/JanelaJogo.java
@@ -1,1 +1,14 @@
+package view;
+import javax.swing.JFrame;
 
+public class JanelaJogo extends JFrame {
+	private static final long serialVersionUID = -4127484169536275339L;
+	
+	public JanelaJogo() {
+		super();
+		setSize(300, 200);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+}
