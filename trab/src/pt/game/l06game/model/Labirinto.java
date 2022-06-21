206695c5b5211048bf6820600f956efc8eb0ab7c
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-06-21 20:48:12 -0300

    Update Labirinto.java

--- trab/src/pt/game/l06game/model/Labirinto.java
+++ trab/src/pt/game/l06game/model/Labirinto.java
@@ -12,6 +12,30 @@ public class Labirinto {
 		}
 	}
 	
+	public void verificarMovimento(String command) {
+		for (int i = 0; i < 19; i++) {
+			for (int j = 0; j < 19; j++) {
+				if (command.equals("w")) {
+					if (maze[i - 1][j] != null) {
+						
+					}
+				} else if (command.equals("s")) {
+					if (maze[i + 1][j] != null) {
+						
+					}
+				} else if (command.equals("a")) {
+					if (maze[i][j - 1] != null) {
+						
+					}
+				} else if (command.equals("d")) {
+					if (maze[i][j + 1] != null) {
+						
+					}
+				}
+			}
+		}
+	}
+	
 	public Celula[][] getMaze() {
 		return maze;
 	}
