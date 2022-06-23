3abc596d16e5634abfba9defe8500aacdd0e734d
MurilloTito <101833515+MurilloTito@users.noreply.github.com>
2022-06-23 12:26:48 -0300

    Update AppCatRinto.java

--- trab/src/pt/game/l06game/main/AppCatRinto.java
+++ trab/src/pt/game/l06game/main/AppCatRinto.java
@@ -1,10 +1,11 @@
-import view.JanelaJogo;
+package main;
+import view.Teclado;
 import model.*;
 import pt.game.l06game.controller.*;
 
 public class AppCatRinto {
 	
 	public static void main(String args[]) {
-		new JanelaJogo();
+		new Teclado();
 	}
 }
