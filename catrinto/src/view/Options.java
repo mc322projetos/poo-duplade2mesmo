52e471c405277827025109973754aab53ea5e1da
oliveira7250 <isaac052503@gmail.com>
2022-06-30 19:52:47 -0300

    .

--- catrinto/src/view/Options.java
+++ catrinto/src/view/Options.java
@@ -15,7 +15,7 @@ public class Options extends JPanel implements ActionListener { // mudar para JP
     
     public Options () {
         
-        ImageIcon cat = new ImageIcon("trab/src/pt/game/l06game/viewObser/cat.png");
+        ImageIcon cat = new ImageIcon("../../assets/cat.png");
 
         JLabel label = new JLabel();
         label.setVerticalAlignment(JLabel.TOP); // muda a pos de icon+text
