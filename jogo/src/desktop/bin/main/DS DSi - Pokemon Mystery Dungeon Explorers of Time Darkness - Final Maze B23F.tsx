406b4102441249abd6f0d24cb7d353ca0dd90056
oliveira7250 <isaac052503@gmail.com>
2022-06-30 19:26:38 -0300

    .

--- jogo/src/desktop/bin/main/DS DSi - Pokemon Mystery Dungeon Explorers of Time Darkness - Final Maze B23F.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="DS DSi - Pokemon Mystery Dungeon Explorers of Time Darkness - Final Maze B23F" tilewidth="20" tileheight="20" tilecount="1672" columns="44">
- <image source="DS DSi - Pokemon Mystery Dungeon Explorers of Time Darkness - Final Maze B23F.png" width="888" height="770"/>
-</tileset>
