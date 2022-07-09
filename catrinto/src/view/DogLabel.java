54ae1369740c8604cd8e56f12fc142aa5c82c866
oliveira7250 <isaac052503@gmail.com>
2022-07-08 21:43:13 -0300

    .

---
+++ catrinto/src/view/DogLabel.java
@@ -0,0 +1,23 @@
+package view;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class DogLabel extends JLabel{
+    MainScreen mainScreen;
+
+    DogLabel(MainScreen mainScreen) {
+        this.mainScreen = mainScreen;
+    }
+
+    public void moverCachorro(int old_i, int old_j, int i, int j) {
+        JLabel empty = (JLabel) mainScreen.getPanel().getComponent(old_i * 20 + old_j);
+        empty.setIcon(new ImageIcon
+        (new ImageIcon("src/view/empty.png").getImage().getScaledInstance(20, 20,  java.awt.Image.SCALE_SMOOTH)));
+
+        JLabel dog = (JLabel) mainScreen.getPanel().getComponent(i * 20 + j);
+        dog.setIcon(new ImageIcon
+        (new ImageIcon("src/view/dog.png").getImage().getScaledInstance(20, 20,  java.awt.Image.SCALE_SMOOTH)));
+        
+    }
+} 
